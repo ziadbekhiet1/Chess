046792c722ee2dc2158252909686fb40de37aa21
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-25 01:26:16 +0000

    Queen move method

--- Chess/src/chess/Queen.java
+++ Chess/src/chess/Queen.java
@@ -9,6 +9,22 @@ public class Queen extends ChessPiece {
 	
 	public boolean move(Board chessboard, int destRow, int destCol) {
 		
-		return false;
+		Space loc = this.getLocation();
+        Space dest = chessboard.getSpace(destRow, destCol);
+
+        System.out.println("Piece color: " + this.getColor());
+        int distanceRow = loc.getRow() - destRow; //x distance between
+        int distanceCol = loc.getCol() - destCol; // y distance between
+        int locRow = loc.getRow(); //x distance between
+        int locCol = loc.getCol(); // y distance between
+
+        if (locRow == destRow || locCol == destCol) {
+            return true;
+        }
+        if (Math.abs(distanceRow) - Math.abs(distanceCol) == 0) { // y=x line so abs of both have to be 0
+            return true;
+        }
+
+        return false;
 	}
 }
