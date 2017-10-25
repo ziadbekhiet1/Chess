1aa8bd07f22f576d8d7afd50721be86de34db13f
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-25 01:29:08 +0000

    Bishop move method

--- Chess/src/chess/Bishop.java
+++ Chess/src/chess/Bishop.java
@@ -9,7 +9,18 @@ public class Bishop extends ChessPiece {
 	
 	public boolean move(Board chessboard, int destRow, int destCol) {
 		
-		return false;
+		Space loc = this.getLocation();
+        Space dest = chessboard.getSpace(destRow, destCol);
+
+        System.out.println("Piece color: " + this.getColor());
+
+        int distanceRow = loc.getRow() - destRow; //x distance between
+        int distanceCol = loc.getCol() - destCol; // y distance between
+        if (Math.abs(distanceRow) - Math.abs(distanceCol) == 0) { // y=x line so abs of both have to be 0
+            return true;
+        }
+
+        return false;
 	}
 
 }
