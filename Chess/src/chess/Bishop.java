87a1877c0d9ee4b9a78829d07c7c90a03a20d157
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-25 02:16:03 +0000

    Bishop.java edited online with Bitbucket

--- Chess/src/chess/Bishop.java
+++ Chess/src/chess/Bishop.java
@@ -9,15 +9,17 @@ public class Bishop extends ChessPiece {
 	
 	public boolean move(Board chessboard, int destRow, int destCol) {
 		
-		Space loc = this.getLocation();
+        Space loc = this.getLocation();
         Space dest = chessboard.getSpace(destRow, destCol);
 
         System.out.println("Piece color: " + this.getColor());
 
         int distanceRow = loc.getRow() - destRow; //x distance between
         int distanceCol = loc.getCol() - destCol; // y distance between
-        if (Math.abs(distanceRow) - Math.abs(distanceCol) == 0) { // y=x line so abs of both have to be 0
-            return true;
+        if (!dest.getOccupied() || dest.getBW() != this.getColor()) {
+            if (Math.abs(distanceRow) - Math.abs(distanceCol) == 0) {// y=x line so abs of both have to be 0
+                return true;
+            }
         }
 
         return false;
