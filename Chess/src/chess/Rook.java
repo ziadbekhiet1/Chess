3718cae1ced033b53ae7445e5157bc10a1c321f6
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-25 02:15:18 +0000

    Rook.java edited online with Bitbucket

--- Chess/src/chess/Rook.java
+++ Chess/src/chess/Rook.java
@@ -9,15 +9,16 @@ public class Rook extends ChessPiece {
 	
 	public boolean move(Board chessboard, int destRow, int destCol) {
 		
-		Space loc = this.getLocation();
+        Space loc = this.getLocation();
         Space dest = chessboard.getSpace(destRow, destCol);
 
         System.out.println("Piece color: " + this.getColor());
         int locRow = loc.getRow(); //x distance between
         int locCol = loc.getCol(); // y distance between
-
-        if (locRow == destRow || locCol == destCol) {
-            return true;
+        if (!dest.getOccupied() || dest.getBW() != this.getColor()) {
+            if (locRow == destRow || locCol == destCol) {
+                return true;
+            }
         }
 
         return false;
