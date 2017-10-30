8c215f012f73fd67a0fedbe56ba800b21e2c0728
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-30 00:18:13 +0000

    ChessPiece.java edited online with Bitbucket

--- Chess/src/chess/ChessPiece.java
+++ Chess/src/chess/ChessPiece.java
@@ -2,29 +2,31 @@ package chess;
 
 public abstract class ChessPiece {
 
-	private char color;
-	private Space location;
-	
-	public ChessPiece(Space location, char color) {
-		
-		this.location = location;
-		this.color = color;
-	}
-	
-	public char getColor() {
-		
-		return this.color;
-	}
-	
-	public Space getLocation() {
-		
-		return this.location;
-	}
-	
-	public void setLocation(Space location) {
-		
-		this.location = location;
-	}
-	
-	public abstract boolean move(Board chessboard, int destRow, int destCol);
+    private char color;
+    private Space location;
+
+    public ChessPiece(Space location, char color) {
+
+        this.location = location;
+        this.color = color;
+    }
+
+    public char getColor() {
+
+        return this.color;
+    }
+
+    public Space getLocation() {
+
+        return this.location;
+    }
+
+    public void setLocation(Space location) {
+
+        this.location = location;
+    }
+
+    public abstract boolean move(Board chessboard, int destRow, int destCol);
+    public abstract void incrementMoved();
+    public abstract int getMoved();
 }
