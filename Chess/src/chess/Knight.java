62afd164935a294578cfaefa510809fedecb552d
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-30 00:13:19 +0000

    Knight.java edited online with Bitbucket

--- Chess/src/chess/Knight.java
+++ Chess/src/chess/Knight.java
@@ -2,13 +2,16 @@ package chess;
 
 public class Knight extends ChessPiece {
 
-	public Knight(Space location, char color) {
-		
-		super(location, color);
-	}
-	
-	public boolean move(Board chessboard, int destRow, int destCol) {
-		
+    private int moved;
+
+    public Knight(Space location, char color) {
+        super(location, color);
+
+        moved = 0;
+    }
+
+    public boolean move(Board chessboard, int destRow, int destCol) {
+
         Space loc = this.getLocation();
         Space dest = chessboard.getSpace(destRow, destCol);
 
@@ -20,11 +23,17 @@ public class Knight extends ChessPiece {
         int locCol = loc.getCol(); // y distance between
         if (!dest.getOccupied() || dest.getBW() != this.getColor()) {
             if (Math.abs(distanceRow) + Math.abs(distanceCol) == 3) {
-               if (locRow != destRow || locCol != destCol) {
-                return true;
+                if (locRow != destRow || locCol != destCol) {
+                    return true;
+                }
             }
         }
-     }
         return false;
-	}
+    }
+    public void incrementMoved() {
+        moved++;
+    }
+    public int getMoved() {
+        return moved;
+    }
 }
