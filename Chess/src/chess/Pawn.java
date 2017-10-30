3a81ddf457597e272b6a2ec8c7f1f39783cdfcab
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-30 00:10:06 +0000

    Pawn.java edited online with Bitbucket

--- Chess/src/chess/Pawn.java
+++ Chess/src/chess/Pawn.java
@@ -2,35 +2,85 @@ package chess;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Space location, char color) {
-		
-		super(location, color);
-	}
-	
-	public boolean move(Board chessboard, int destRow, int destCol) {
-		
-		Space loc = this.getLocation();
-		Space dest = chessboard.getSpace(destRow, destCol);
-		
-		System.out.println("Piece color: " + this.getColor());
-		
-		if(this.getColor() == 'w') {
-			
-			// white piece
-			
-			// check basic pawn forward movement constraints
-			if(destRow != loc.getRow() - 1 || destRow != loc.getRow() -2) {
-				
-				System.out.println("ERROR: Pawn can't move more than 2 spaces forward.");
-				return false;
-			}
-			
-		}
-		else {
-			
-			// black piece
-		}
-		
-		return false;
-	}
+    private int moved;
+    private boolean canEmp;
+    private int lastmove;
+
+    public Pawn(Space location, char color) {
+        super(location, color);
+
+        moved = 0;
+        canEmp = false;
+        lastmove = 0;
+    }
+
+    public boolean move(Board chessboard, int destRow, int destCol) {
+
+        Space loc = this.getLocation();
+        Space dest = chessboard.getSpace(destRow, destCol);
+
+        System.out.println("Piece color: " + this.getColor());
+
+        if(this.getColor() == 'w') {
+
+            // white piece
+
+            // check basic pawn forward movement constraints
+            if(destCol == loc.getCol() && (destRow == loc.getRow() + 1 || destRow == loc.getRow() + 2)) {
+
+                System.out.println("ERROR: Pawn can't move more than 2 spaces forward.");
+                return true;
+            }
+            else if (destRow == loc.getRow() + 1 && (destCol == loc.getCol() + 1 || destCol == loc.getCol() - 1)) {
+                if(chessboard.castling(this.getLocation().getRow(), this.getLocation().getCol()) && this.getCanEmp()) {
+                    return true;
+                }
+                if (dest.getOccupied())
+                    return true;
+            }
+
+        }
+        else {
+
+            if (destCol == loc.getCol() && (destRow == loc.getRow() - 1 || destRow == loc.getRow() - 2)) {
+
+                System.out.println("ERROR: Pawn can't move more than 2 spaces forward.");
+                return true;
+            }
+            else if (destRow == loc.getRow() - 1 && (destCol == loc.getCol() + 1 || destCol == loc.getCol() - 1)) {
+                if (dest.getOccupied())
+                    return true;
+                if(chessboard.castling(this.getLocation().getRow(), this.getLocation().getCol()) && this.getCanEmp()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public void incrementMoved() {
+        moved++;
+    }
+    public int getMoved() {
+        return moved;
+    }
+
+    public void setLastMove(int x) {
+        this.lastmove = x;
+
+    }
+    public int getLastMove() {
+        return this.lastmove;
+    }
+    public void setCanEmp(boolean x) {
+        this.canEmp = x;
+    }
+    public boolean getCanEmp() {
+        return canEmp;
+    }
+
+
+
+
+
 }
