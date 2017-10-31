255d7980580e893120bd0fb3d5757c91e7796986
Ziad Bekhiet <zyb3@scarletmail.rutgers.edu>
2017-10-31 22:58:43 +0000

    King.java edited online with Bitbucket

--- Chess/src/chess/King.java
+++ Chess/src/chess/King.java
@@ -16,22 +16,21 @@ public class King extends ChessPiece {
         Space dest = chessboard.getSpace(destRow, destCol);
 
         System.out.println("Piece color: " + this.getColor());
-
         int distanceRow = start.getRow() - destRow; //x distance between
         int distanceCol = start.getCol() - destCol; // y distance between
-        if (!dest.getOccupied() || dest.getBW() != this.getColor()) {
+        if (dest.getBW() != this.getColor()) {
             if (distanceRow == 0 || distanceRow == 1) {
                 if (distanceCol == 0 || distanceCol == 1) {
                     return true;
                 }
             }
-            if (start.getPiece().getMoved() == 0) {
-                if (destCol == start.getCol() && (Math.abs(distanceRow) == 2) && start.getBW() == 'b') {
-                    if (chessboard.getSpace(0, 0).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getMoved() == 0 && chessboard.getSpace(0, 7).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getMoved() == 0) {
+            if (((King) start.getPiece()).getLastMove() == 0) {
+                if (destRow == start.getRow() && (Math.abs(distanceCol) == 2) && start.getBW() == 'b') {
+                    if (chessboard.getSpace(0, 0).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getLastMove() == 0 && chessboard.getSpace(0, 7).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getLastMove() == 0) {
                         return true;
                     }
                 } else if (destCol == start.getCol() && (Math.abs(distanceRow) == 2) && start.getBW() == 'w') {
-                    if (chessboard.getSpace(7, 0).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getMoved() == 0 && chessboard.getSpace(7, 7).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getMoved() == 0) {
+                    if (chessboard.getSpace(7, 0).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getLastMove() == 0 && chessboard.getSpace(7, 7).getPiece() instanceof Rook && chessboard.getSpace(0, 0).getPiece().getLastMove() == 0) {
                         return true;
                     }
                 }
@@ -39,13 +38,6 @@ public class King extends ChessPiece {
         }
         return false;
     }
-    public void incrementMoved() {
-        moved++;
-    }
-    public int getMoved() {
-        return moved;
-    }
-
     public void setLastMove(int x) {
         this.lastmove = x;
 
